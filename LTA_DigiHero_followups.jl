9c09a643b02c94981196142cff9446b6b12e95ef
Carlotta Gerstein <100771374+charlie0614@users.noreply.github.com>
2026-07-17 10:22:57 +0200

    cleanup main script

--- LTA_DigiHero_followups.jl
+++ LTA_DigiHero_followups.jl
@@ -10,17 +10,15 @@ using PlotlyJS
 
 sim_dir = "data/DigiHero/"
 n_states = 3
-sim_no = string(n_states, "2026_07_01/age_sex_comorbidity_covs_200its")
-combined_all = CSV.read(sim_dir * "2026-07-01_data_symptoms_DigiHero_Bonn_preprocessed_combined.csv", DataFrame, missingstring=["", "NA"]);
-
-symptom_labels = ["fatigue", "exercise_intolerance", "joint_muscle_pain", "chest_pain", "gastrointestinal", "neurological", "sleep_disturbances", "other"]
+sim_no = string("2026_06_02/", n_states, "states/age_sex_comorbidity_covs_200its")
+combined_all = CSV.read(sim_dir * "2026-06-02_data_symptoms_DigiHero_Bonn_preprocessed.csv", DataFrame, missingstring=["", "NA"]);
 
 println("Running simulation with ", sim_no)
 
 function add_dummy_symptom_cols(combined_all)
     for timepoint in ["9months", "15months", "21months", "27months"]
         # Add missing timepoint symptom columns
-        for symptom in symptoms_list
+        for symptom in symptoms_list_fup
             combined_all[!, "pcc_"*timepoint*"_sym_"*symptom] .= -1
         end
     end
@@ -29,100 +27,98 @@ function add_dummy_symptom_cols(combined_all)
     return combined_all
 end
 
-pattern = Regex(join(vcat(symptoms_list, covariates_list_new_agegroups), "|"))
+pattern = Regex(join(vcat(symptoms_list_fup, covariates_list_new_agegroups), "|"))
 preprocessed_df = select(combined_all, names(combined_all, pattern))
 preprocessed_df = preprocessed_df[:, 3:end]
 preprocessed_df = add_dummy_symptom_cols(preprocessed_df)
 
 preprocessed_df = filter(row -> !all(ismissing, row[Not(covariates_list_new_agegroups)]), preprocessed_df)
 
-sim_output = LTA.create_sim_mod_data(preprocessed_df; covs=covariates_list_new_agegroups, bins=symptoms_list, conts=String[], visits=String[], labels=["pcc_" * string(timepoints)*"months_sym_" for timepoints in collect(3:3:30)], n_states=n_states, covariate_tup=(initial=covariates_list_new_agegroups, trans=covariates_list_new_agegroups, em=[]), sim_no=sim_no, comments="")
-# save(sim_dir * sim_no * "/simulation/sim_output.jld2", "sim_output", sim_output)
+sim_output = LTA.create_sim_mod_data(preprocessed_df; covs=covariates_list_new_agegroups, bins=symptoms_list_fup, conts=String[], visits=String[], labels=["pcc_" * string(timepoints)*"months_sym_" for timepoints in collect(3:3:30)], n_states=n_states, covariate_tup=(initial=covariates_list_new_agegroups, trans=covariates_list_new_agegroups, em=[]), sim_no=sim_no, comments="")
+save("outputs/" * sim_no * "/simulation/sim_output.jld2", "sim_output", sim_output)
 
 # ############ ESTIMATION #############
 
 estimation_output = LTA.run_estimation(sim_output; est_seed=1,
     # meth = "GradientDescent",
     meth="BFGS",
-    n_starts=2,
-    iterations=5,
-    store_trace=true,
-    extended_trace = true,
-    show_trace = false)
+    n_starts=20,
+    iterations=50)
 
-# save(sim_dir * sim_no * "/results/estimates/est_output.jld2", "estimation_output", estimation_output)
+save("outputs/" * sim_no * "/results/estimates/est_output.jld2", "estimation_output", estimation_output)
 
-# estimation_output = load("estimates/" * sim_no * "/results/estimates/est_output.jld2", "estimation_output")
+# estimation_output = load("outputs/" * sim_no * "/results/estimates/est_output.jld2", "estimation_output")
 
-# sim_output, sim_params, sim_hyper = LTA.unpack(estimation_output)
-# covariate_idx = LTA.convert_covariate_2_df_indices(
-#     sim_params.covariate_mat_headers,
-#     sim_hyper.covariate_tup
-# )
+mkpath("outputs/" * sim_no * "/results/plots")
 
-# best_model_params = LTA.get_model_params(estimation_output, type="best_fitted")
+sim_output, sim_params, sim_hyper = LTA.unpack(estimation_output)
+covariate_idx = LTA.convert_covariate_2_df_indices(
+    sim_params.covariate_mat_headers,
+    sim_hyper.covariate_tup
+)
 
-# fig_em = PlotlyJS.make_subplots(
-#     rows=1, cols=1
-# )
-# em_states = LTA.get_bernoulli_probs(best_model_params.emissions.beta_bernoulli, sim_params.covariate_mat[1, :][covariate_idx[:em]])
-# true_heat_trace = LTA.plot_single_bernoulli_probs(em_states; symptom_labels=symptom_cluster_list)
-# add_trace!(fig_em, true_heat_trace, row=1, col=1)
-# PlotlyJS.savefig(fig_em, "estimates/"* sim_no * "/emission_matrix.png", width=600, height=400)
+best_model_params = LTA.get_model_params(estimation_output, type="best_fitted")
 
-# titles = reshape(["Emission matrix", "Transition matrix", "Initial States"], 1, 3)
-# fig_em_trans = PlotlyJS.make_subplots(
-#     rows=1, cols=3, column_widths=[0.4, 0.4, 0.2], subplot_titles=titles
-# )
-# em_states = LTA.get_bernoulli_probs(best_model_params.emissions.beta_bernoulli, sim_params.covariate_mat[1, :][covariate_idx[:em]])
-# true_heat_trace = LTA.plot_single_bernoulli_probs(em_states; symptom_labels)
-# add_trace!(fig_em_trans, true_heat_trace, row=1, col=1)
-# transition_probs = LTA.get_rho_beta_transition_mat(
-#     best_model_params.beta_transition, best_model_params.rho_trans, sim_params.covariate_mat[1, :])
-#     transition_probs = LTA.get_rho_beta_transition_mat(
-#     best_model_params.beta_transition, best_model_params.rho_trans, sim_params.covariate_mat[1, :])
-# true_heat_trace = LTA.plot_single_transition_mat(transition_probs)
-# add_trace!(fig_em_trans, true_heat_trace, row=1, col=2)#, name="Transition matrix")
-# initial_states = LTA.get_rho_beta_initial_states(
-#     best_model_params.beta_initial,
-#     best_model_params.rho_initial,
-#     sim_params.covariate_mat[1, :][covariate_idx[:initial]])
-# true_heat_trace = LTA.plot_single_initial(initial_states)
-# add_trace!(fig_em_trans, true_heat_trace, row=1, col=3)#, name="Initial States")
-# relayout!(fig_em_trans,
-#     xaxis_showticklabels=true,
-#     xaxis2_showticklabels=true,
-#     xaxis3_showticklabels=false
-# )
-# PlotlyJS.savefig(fig_em_trans, "estimates/"* sim_no * "/heatmaps.png", width=1500, height=400)
+fig_em = PlotlyJS.make_subplots(
+    rows=1, cols=1
+)
+em_states = LTA.get_bernoulli_probs(best_model_params.emissions.beta_bernoulli, sim_params.covariate_mat[1, :][covariate_idx[:em]])
+true_heat_trace = LTA.plot_single_bernoulli_probs(em_states; symptom_labels=symptoms_list_fup)
+add_trace!(fig_em, true_heat_trace, row=1, col=1)
+PlotlyJS.savefig(fig_em, "outputs/" * sim_no * "/results/plots/emission_matrix.png", width=600, height=400)
 
-# obs = sim_output.observations.bernoulli_observations
+titles = reshape(["Emission matrix", "Transition matrix", "Initial States"], 1, 3)
+fig_em_trans = PlotlyJS.make_subplots(
+    rows=1, cols=3, column_widths=[0.4, 0.4, 0.2], subplot_titles=titles
+)
+em_states = LTA.get_bernoulli_probs(best_model_params.emissions.beta_bernoulli, sim_params.covariate_mat[1, :][covariate_idx[:em]])
+true_heat_trace = LTA.plot_single_bernoulli_probs(em_states; symptom_labels=symptoms_list_fup)
+add_trace!(fig_em_trans, true_heat_trace, row=1, col=1)
+transition_probs = LTA.get_rho_beta_transition_mat(
+    best_model_params.beta_transition, best_model_params.rho_trans, sim_params.covariate_mat[1, :])
+    transition_probs = LTA.get_rho_beta_transition_mat(
+    best_model_params.beta_transition, best_model_params.rho_trans, sim_params.covariate_mat[1, :])
+true_heat_trace = LTA.plot_single_transition_mat(transition_probs)
+add_trace!(fig_em_trans, true_heat_trace, row=1, col=2)#, name="Transition matrix")
+initial_states = LTA.get_rho_beta_initial_states(
+    best_model_params.beta_initial,
+    best_model_params.rho_initial,
+    sim_params.covariate_mat[1, :][covariate_idx[:initial]])
+true_heat_trace = LTA.plot_single_initial(initial_states)
+add_trace!(fig_em_trans, true_heat_trace, row=1, col=3)#, name="Initial States")
+relayout!(fig_em_trans,
+    xaxis_showticklabels=true,
+    xaxis2_showticklabels=true,
+    xaxis3_showticklabels=false
+)
+PlotlyJS.savefig(fig_em_trans, "outputs/" * sim_no * "/results/plots/heatmaps.png", width=1500, height=400)
 
-# fig1 = LTA.compare_estimation_2_data_fup(estimation_output; T=10, symptom_names=symptom_cluster_list, type="best_fitted", observation_type="bernoulli")
-# fig1_with_CI = LTA.compare_estimation_2_data_with_CIs_fup(estimation_output; T=10, symptom_names=symptom_cluster_list, type="best_fitted", observation_type="bernoulli")
-# PlotlyJS.savefig(fig1, "estimates/"* sim_no * "/compare_est_2_data.png", width=1500, height=800)
-# PlotlyJS.savefig(fig1_with_CI, "estimates/"* sim_no * "/compare_est_2_data_with_CIs.png", width=1500, height=800)
-# fig2 = LTA.compare_bernoulli_heatmaps(estimation_output)
-# PlotlyJS.savefig(fig2, "estimates/"* sim_no * "/compare_bernoulli_heatmaps.png", width=4000, height=400)
-# fig3 = LTA.compare_rhos(estimation_output)
-# PlotlyJS.savefig(fig3, "estimates/"* sim_no * "/compare_rhos.png")
+obs = sim_output.observations.bernoulli_observations
 
-# waterfall = LTA.waterfall_plot(estimation_output)
-# PlotlyJS.savefig(waterfall, "estimates/"* sim_no * "/waterfall.png")
+fig1 = LTA.compare_estimation_2_data_fup(estimation_output; T=10, symptom_names=symptoms_list_fup, type="best_fitted", observation_type="bernoulli")
+fig1_with_CI = LTA.compare_estimation_2_data_with_CIs_fup(estimation_output; T=10, symptom_names=symptoms_list_fup, type="best_fitted", observation_type="bernoulli")
+PlotlyJS.savefig(fig1, "outputs/" * sim_no * "/results/plots/compare_est_2_data.png", width=1500, height=800)
+PlotlyJS.savefig(fig1_with_CI, "outputs/" * sim_no * "/results/plots/compare_est_2_data_with_CIs.png", width=1500, height=800)
+fig2 = LTA.compare_bernoulli_heatmaps(estimation_output)
+PlotlyJS.savefig(fig2, "outputs/" * sim_no * "/results/plots/compare_bernoulli_heatmaps.png", width=4000, height=400)
+fig3 = LTA.compare_rhos(estimation_output)
+PlotlyJS.savefig(fig3, "outputs/" * sim_no * "/results/plots/compare_rhos.png")
 
-# range = LTA.extract_rtrans_hist(best_model_params.rho_trans, sim_params.covariate_mat)
-# range = LTA.extract_rtrans_range(best_model_params.rho_trans, sim_params.covariate_mat)
+waterfall = LTA.waterfall_plot(estimation_output)
+PlotlyJS.savefig(waterfall, "outputs/" * sim_no * "/results/plots/waterfall.png")
 
-# println(range)
-# fig4 = LTA.plot_transition_matrices(best_model_params.beta_transition, range)
-# PlotlyJS.savefig(fig4, "estimates/"* sim_no * "/transition_matrices_covs_test.png", width = 1200, height=750)
+range = LTA.extract_rtrans_hist(best_model_params.rho_trans, sim_params.covariate_mat)
+range = LTA.extract_rtrans_range(best_model_params.rho_trans, sim_params.covariate_mat)
 
-# fig5 = LTA.plot_initial_probs(best_model_params.beta_initial, range)
-# PlotlyJS.savefig(fig5, "estimates/"* sim_no * "/initial_probs_covs.png", width = 500, height=1000)
+println(range)
+fig4 = LTA.plot_transition_matrices(best_model_params.beta_transition, range)
+PlotlyJS.savefig(fig4, "outputs/" * sim_no * "/results/plots/transition_matrices_covs_test.png", width = 1200, height=750)
+
+fig5 = LTA.plot_initial_probs(best_model_params.beta_initial, range)
+PlotlyJS.savefig(fig5, "outputs/" * sim_no * "/results/plots/initial_probs_covs.png", width = 500, height=1000)
 
 
-# # Ensemble-based uncertainty quantification 
-# println(length(estimation_output.fitted_model_params))
-# fig = LTA.ensemble_uq_fup(estimation_output; T=10, symptom_names=symptom_cluster_list, observation_type="bernoulli", num_multistarts=5)
-# PlotlyJS.savefig(fig, "estimates/"* sim_no * "/ensemble_uq.png", width=1500, height=800)
+# Ensemble-based uncertainty quantification 
+fig = LTA.ensemble_uq_fup(estimation_output; T=10, symptom_names=symptoms_list_fup, observation_type="bernoulli", num_multistarts=10)
+PlotlyJS.savefig(fig, "outputs/" * sim_no * "/results/plots/ensemble_uq.png", width=1500, height=800)
 
--- digihero_params.jl
+++ digihero_params.jl
@@ -6,16 +6,71 @@ symptoms_list = [
     "shortness_of_breath",
     "fatigue",
     "headache",
-    "cognitive_impairment"]
+    "cognitive_impairment", 
+    "arrythmia", 
+    "chestpain", 
+    "cold", 
+    "depression", 
+    "diarrhea", 
+    "eye_conjunctivitis",
+    "fever",
+    "lymph_node_swelling",
+    "nausea",
+    "night_sweat",
+    "pms",
+    "sleep_disorder",
+    "sore_throat",
+    "symptoms_ear_pain",
+    "vertigo"]
 
 symptom_cluster_list = ["fatigue", "exercise_intolerance", "joint_muscle_pain", "chest_pain", "gastrointestinal", "neurological", "sleep_disturbances", "other"]
+
+symptoms_list_fup = [
+    "abdomen_pain", 
+    "arrhythmia",
+    "diarrhea",
+    "fatigue",
+    "headache",
+    "joint_muscle_pain",
+    "nausea",
+    "pms",
+    "shortness_of_breath",
+    "sleep_disorder",
+    "vertigo"]
+
+# symptoms_list_fup = [
+#     "abdomen_pain", 
+#     "arrythmia", 
+#     "back_pain", 
+#     "chest_pain", 
+#     "circulatory_problems", 
+#     "concentration_problems", 
+#     "diarrhea",
+#     "fainting_spell",
+#     "fatigue",
+#     "headache",
+#     "joint_muscle_pain",
+#     "light_sensitivity",
+#     "low_performance",
+#     "memory_impairment",
+#     "muscle_twitching", 
+#     "muscle_weakness",
+#     "nausea",
+#     "pain_intercourse",
+#     "pms",
+#     "rapid_exhaustion",
+#     "sensory_disturbances",
+#     "shortness_of_breath",
+#     "sleep_disorder",
+#     "vertigo"]
 
 
 covariates_list = ["female", "30-39", "40-59", "60+", "alpha", "delta", "omicron"]
 # covariates_list = ["female", "30-39", "40-59", "60+", "comorbidity"]
 covariates_list_wo_comorbidities_new_agegroups = ["female", "30-59", "60+"]
 covariates_list_wo_comorbidities = ["female", "30-39", "40-59", "60+"]
-covariates_list_new_agegroups = ["female", "35-59", "60-79", "80+", "comorbidity"]
+# covariates_list_new_agegroups = ["female", "35-59", "60-79", "80+", "comorbidity"]
+covariates_list_new_agegroups = ["female", "35-59", "60-79", "comorbidity"]
 
 
 # covariates_list = String[]
